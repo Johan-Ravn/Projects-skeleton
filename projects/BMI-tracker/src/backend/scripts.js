12f6987809b98fcf461b936308b84e4173c43150
Johan-Ravn <jran22@student.aau.dk>
2023-06-02 23:10:20 +0200

    Update

---
+++ projects/BMI-tracker/src/backend/scripts.js
@@ -0,0 +1,10 @@
+
+function extractDrinkData() {
+    let drinkData = document.getElementById("name_id")
+    let maleOption = document.getElementById("male_id")
+    let femaleOption = document.getElementById("female_id")
+
+    if (maleOption.checked) {
+
+    }
+}
